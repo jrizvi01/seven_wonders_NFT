927073edbcfc5dd66530590ad5a4eba9af29aa2a
You <80657579+Jyou965@users.noreply.github.com>
2021-08-25 15:02:36 -0500

    Part 1

---
+++ contracts/wonders.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.5;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
+
+contract WondersToken is ERC721Full {
+    constructor() public ERC721Full("WondersToken", "WDRS") {}
+
+    function registerSite(address owner, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 tokenId = totalSupply();
+        _mint(owner, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+
+        return tokenId;
+    }
+}
