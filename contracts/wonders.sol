3710e9a421c27624a53d8f62f8e5e6928881b0e8
Carlos Raymundo <reachcarlostoday@gmail.com>
2021-08-26 10:21:38 -0700

    Defining the Wonders data structure - completed

--- contracts/wonders.sol
+++ contracts/wonders.sol
@@ -2,17 +2,45 @@ pragma solidity ^0.5.5;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
 
-contract WondersToken is ERC721Full {
-    constructor() public ERC721Full("WondersToken", "WDRS") {}
+contract ArtRegistry is ERC721Full {
+    constructor() public ERC721Full("ArtRegistryToken", "ART") {}
 
-    function registerSite(address owner, string memory tokenURI)
-        public
-        returns (uint256)
-    {
+    struct Site {
+        string name;
+        string artist;
+        uint256 appraisalValue;
+    }
+
+    mapping(uint256 => Site) public artCollection;
+
+    event Appraisal(uint256 tokenId, uint256 appraisalValue, string reportURI);
+
+    function registerSite(
+        address owner,
+        string memory name,
+        string memory artist,
+        uint256 initialAppraisalValue,
+        string memory tokenURI
+    ) public returns (uint256) {
         uint256 tokenId = totalSupply();
+
         _mint(owner, tokenId);
         _setTokenURI(tokenId, tokenURI);
 
+        artCollection[tokenId] = Site(name, artist, initialAppraisalValue);
+
         return tokenId;
+    }
+
+    function newAppraisal(
+        uint256 tokenId,
+        uint256 newAppraisalValue,
+        string memory reportURI
+    ) public returns (uint256) {
+        artCollection[tokenId].appraisalValue = newAppraisalValue;
+
+        emit Appraisal(tokenId, newAppraisalValue, reportURI);
+
+        return artCollection[tokenId].appraisalValue;
     }
 }
